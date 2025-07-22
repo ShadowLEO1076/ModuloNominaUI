2ac53048dc306d5e08daecf9bfff4b09ab6d6689
MateoVasquezUisrael <e1752779908@uisrael.edu.ec>
2025-07-22 15:45:42 -0500

    Se completo FormEmpleados

--- WinModuloNomina/Program.cs
+++ WinModuloNomina/Program.cs
@@ -13,7 +13,7 @@ namespace WinModuloNomina
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MenuPrincipal());
+            Application.Run(new F2Empleados());
         }
     }
 }
